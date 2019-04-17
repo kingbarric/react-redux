09b98634430fdbc2b1119f9fb48a12e34b3a6ee0
Vonmishka <toju2031@gmail.com>
2019-04-17 20:18:33 +0100

    okay

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {connect} from 'react-redux' 
+import {updateUser} from './actions/userAction'
+import { stat } from 'fs';
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+  onUpdateUser(){
+    this.props.onUpdateUser('Jenifer mathew')
+  }
   render() {
+    console.log(this.props)
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +22,15 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <div>
+            <button onClick={this.onUpdateUser} >Update user</button>
+          </div>
+          <div>
+            {this.props.user}
+          </div>
+          <div>
+            {this.props.products[0].name}
+          </div>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -24,5 +44,17 @@ class App extends Component {
     );
   }
 }
+const mapStateToProps = (state, props) =>{
+  //console.log(props)
+ return {
+   products: state.products,
+  user: state.user,
+  userPlusProps: `${state.user} ${props.aProps}`
+ } 
+}
 
-export default App;
+const mapActionToProps ={
+  onUpdateUser: updateUser
+}
+
+export default connect(mapStateToProps,mapActionToProps) (App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from 'redux';
+import {Provider} from 'react-redux';
+import productReducer from './reducers/productReducers';
+import userReducer from './reducers/userReducers';
+// function reducer(state,action){
+//     if(action.type==='changeState'){
+//         return action.payload.newState;
+//     }
+//     return 'State'
+// }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const allReducers = combineReducers({
+    products: productReducer,
+    user: userReducer
+});
+
+// const store = createStore(reducer);
+const store = createStore(
+    allReducers, {
+        products: [{ name: 'iphone' }, { description: 'wak phone' }],
+        user: 'Micheal'
+    },
+    
+    window.devToolsExtension && window.devToolsExtension() 
+    );
+
+// const action = {
+//     type: 'changeState',
+//     payload:{
+//         newState: 'My new state'
+//     }
+// }
+
+//store.dispatch(action);
+const updateUserAction = {
+    type: 'updateUser',
+    payload:{
+        user:'Eric Tyler'
+    }
+}
+store.dispatch(updateUserAction);
+console.log(store.getState())
+ReactDOM.render(<Provider store={store}><App aProps="Some kings props" /> </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
